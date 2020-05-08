970a1571fe847623b138b10b13cf1f9a5c5155a5
Shubham Shekhar <shubhamshekhar123.ss@gmail.com>
2020-05-08 10:43:32 +0530

    update mock test

--- testing/finder.test.js
+++ testing/finder.test.js
@@ -1,10 +1,14 @@
 const finder=require('../index')
 
 const find_server=finder.findServer
+const fetch=require('node-fetch')
+
+jest.mock('node-fetch', ()=>jest.fn())
 
 
 describe("Testing for multiple servers",()=>{
     it('testing when all server is offline',()=>{
+        fetch.mockResolvedValue(null,null);
         return find_server([
             {
                 "url": "http://doesNotExist.boldtech.co",
@@ -19,10 +23,11 @@ describe("Testing for multiple servers",()=>{
         })
     })
     it('testing for if server is send on priority',()=>{
+        fetch.mockResolvedValue({'status':200},{'status':201},{'status':202});
         return find_server([
         {
             "url": "https://filesshar.herokuapp.com/",
-            "priority": 1
+            "priority": 17
         },
         {
             "url": "http://boldtech.co",
@@ -33,10 +38,11 @@ describe("Testing for multiple servers",()=>{
             "priority": 4
         }
         ]).then(data=>{
-            expect(data.url).toBe('https://filesshar.herokuapp.com/')
+            expect(data.url).toBe('http://google.com')
         })
     })
     it('testing for if only one server is passed',()=>{
+        fetch.mockResolvedValue({ status: 200 });
         return find_server({
             "url": "http://google.com",
             "priority": 4
@@ -45,6 +51,7 @@ describe("Testing for multiple servers",()=>{
         })
     })
     it('testing for if only one server is passed and is offline',()=>{
+        fetch.mockResolvedValue(null);
         return find_server({
             "url": "http://doesNotExist.boldtech.co",
             "priority": 4
